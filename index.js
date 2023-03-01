080bbf2b4b4995a9e22fe809526bbf5e59390ec0
Saurav <chatterjee.saurav123@gmail.com>
2023-03-01 12:23:43 +0530

    design changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-area");
 var output_div=document.querySelector("#output");
+var after_click=document.querySelector("#afterclick");
 
 var serverURL="https://api.funtranslations.com/translate/minion.json";
 
@@ -12,28 +13,27 @@ function getTranslatinURL(text){
  function errorHandler(error){
     console.log("error occured" , error);
     alert("Server is not working, try later");
-
  }
 
-
-
-
-function clickHandler(){
-    // output_div.innerText= "abcdefghtuijk   " + txtInput.value;
+ 
+const clickHandler=() => {
+  
     var inputText=txtInput.value; //taking input
+    txtInput.value="";
+    after_click.innerHTML=inputText;
 
     // calling server for processing
     fetch(getTranslatinURL(inputText))
        .then(response => response.json())
        .then(json =>{
          var translatedText= json.contents.translated;
-         output_div.innerText=translatedText;// output
+         output_div.innerText=translatedText;  // output
          console.log("Fine")
        })
         .catch(errorHandler)
 }
 
-btnTranslate.addEventListener("click",clickHandler);
+ btnTranslate.addEventListener("click",clickHandler);
 
 
 
